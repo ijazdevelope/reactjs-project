dee1c9d04251a5df3e85d96eb57af6667753df0b
Muhammad Ijaz <muhammad.ijaz@codilitysolutions.com>
2022-01-21 16:47:59 +0500

    installed @hookform/resolvers yup and applied validation on login.js page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
+
+const schema = yup.object({
+    email: yup.string().required(),
+    password: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+}).required();
 
 const Login = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data, 'data');
-
-    // const handleSubmit =()=>{
-    //     console.log('handleSubmit');
-    // }
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
+    const onSubmit = data => console.log(data,'data');
+    // console.log(formState, 'errors');
 
     return (
         <div className="col-md-6 grid place-content-center vh-100">
@@ -17,16 +24,14 @@ const Login = () => {
             <form onSubmit={handleSubmit(onSubmit)} >
                 <div className='row'>
                     <div className="form-floating mb-3 w-100 ps-0">
-                        <input type="email" {...register("email", { required: true })} className={`form-control position-relative ${errors.email ? 'border-danger' : 'border'}`} id="floatingInput" placeholder="name@example.com" />
+                        <input type="email" {...register("email")} className='form-control position-relative' id="floatingInput" placeholder="name@example.com" />
                         <label for="floatingInput">Email address</label>
-                        {errors.email && <i className="bi bi-x-lg text-danger position-absolute right-5 top-4"></i>}
-                        {errors.email && <span className='p-0 text-danger'>Email address is required</span>}
+                        <p className='text-danger'>{errors.email?.message}</p>
                     </div>
                     <div className="form-floating w-100 ps-0">
-                        <input type="password" {...register("password", { required: true })} className={`form-control position-relative ${errors.password ? 'border-danger' : 'border'}`} id="floatingPassword" placeholder="Password" />
+                        <input type="password" {...register("password")} className='form-control position-relative' id="floatingPassword" placeholder="Password" />
                         <label for="floatingPassword">Password</label>
-                        {errors.password && <i className="bi bi-x-lg text-danger position-absolute right-5 top-4"></i>}
-                        {errors.password && <span className='p-0 text-danger'>Password is required</span>}
+                        <p className='text-danger'>{errors.password?.message}</p>
                     </div>
                     <button type="submit" className="btn btn-primary mt-3 col-2">Login</button>
                 </div>
