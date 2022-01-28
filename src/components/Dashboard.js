6e72a5f88b6f20830f63455488fd3705025314b2
Muhammad Ijaz <muhammad.ijaz@codilitysolutions.com>
2022-01-28 11:07:30 +0500

    applied react-chart-js and created a new component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Navbar from "./Navbar";
+import MyChart from "./MyChart";
 
 const Dashboard = () => {
-    return (
-        <div className='w-100'>
-            <div>
-                <Navbar />
-            </div>
-            <div className='text-center fs-1 text-primary'>
-                My Dashboard
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="w-100">
+      <div>
+        <Navbar />
+      </div>
+      <div className="text-center fs-1 text-primary w-100 h-50">
+        <h1>Doughnut Chart</h1>
+        <MyChart />
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
