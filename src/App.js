3c3f14f88627fce3f9ffecfdac456107a43c9a48
Muhammad Ijaz <muhammad.ijaz@codilitysolutions.com>
2022-02-02 10:05:20 +0500

    add css styling for responsive gender field

--- src/App.js
+++ src/App.js
@@ -14,20 +14,24 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 
+const ThemeContext = React.createContext();
+
 function App() {
   return (
     <>
-      <div className="h-100 grid place-items-center">
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='dashboard' element={<Dashboard />} />
-          <Route path='signup' element={<Signup />} />
-          <Route path='home' element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='portfolio' element={<Portfolio />} />
-        </Routes>
-      </div>
+      <ThemeContext.Provider>
+        <div className="h-100 grid place-items-center">
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path='home' element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='portfolio' element={<Portfolio />} />
+          </Routes>
+        </div>
+      </ThemeContext.Provider>
     </>
   );
 }
