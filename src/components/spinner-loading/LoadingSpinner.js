66c20490de1352394d4039c6c6d47643e05dd011
Muhammad Ijaz <ijazlovely00786@gmail.com>
2022-03-11 10:40:36 +0500

    applied styling on spinner-loader

--- src/components/spinner-loading/LoadingSpinner.js
+++ src/components/spinner-loading/LoadingSpinner.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './LoadingSpinner.css';
 
 const LoadingSpinner = () => {
     return (
@@ -6,7 +7,7 @@ const LoadingSpinner = () => {
         <img
          src="https://cdn.dribbble.com/users/1787505/screenshots/7300251/media/a351d9e0236c03a539181b95faced9e0.gif"
          alt="spinner-loader-image" 
-         className=''
+         className='loader_spinner'
          />
             {/* <Spinner animation="border" size="sm" />
             <Spinner animation="border" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ function Home() {
   return (
     <>
       <div className="w-100">
-        <Navbar />
       </div>
       <div className="w-100 d-flex justify-content-between p-3">
         <h2 className="text-center">Home</h2>
@@ -13,6 +12,7 @@ function Home() {
           Logout
         </button>
       </div>
+        <Navbar />
     </>
   );
 }
