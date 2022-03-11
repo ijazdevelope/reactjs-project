4a0face0afe922e7da6006b9d5c6ac20be545d20
Muhammad Ijaz <ijazlovely00786@gmail.com>
2022-03-11 11:22:58 +0500

    add spinner loader

--- src/components/spinner-loading/LoadingSpinner.js
+++ src/components/spinner-loading/LoadingSpinner.js
@@ -4,11 +4,19 @@ import './LoadingSpinner.css';
 const LoadingSpinner = () => {
     return (
         <>
-        <img
-         src="https://cdn.dribbble.com/users/1787505/screenshots/7300251/media/a351d9e0236c03a539181b95faced9e0.gif"
+            {/* <button class="btn btn-primary" type="button" disabled>
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                <span class="sr-only">Loading...</span>
+            </button> */}
+            <button class="btn btn-primary" type="button" disabled>
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Loading...
+            </button>
+            {/* <img
+         src="https://i.gifer.com/ZZ5H.gif"
          alt="spinner-loader-image" 
          className='loader_spinner'
-         />
+         /> */}
             {/* <Spinner animation="border" size="sm" />
             <Spinner animation="border" />
             <Spinner animation="grow" size="sm" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ function Home() {
   return (
     <>
       <div className="w-100">
+        <Navbar />
       </div>
       <div className="w-100 d-flex justify-content-between p-3">
         <h2 className="text-center">Home</h2>
@@ -12,7 +13,6 @@ function Home() {
           Logout
         </button>
       </div>
-        <Navbar />
     </>
   );
 }
